c6e8e0cd993ed101b5db5c82237544a5b1fc57a3
Stef1e <154548870+Stef1e@users.noreply.github.com>
2024-05-09 14:58:42 -0400

    Update for_abe.sh
    💯

--- for_abe.sh
+++ for_abe.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
- # Define text colors
- RED='\033[0;31m'
- GREEN='\033[0;32m'
- YELLOW='\033[1;33m'
- NC='\033[0m' # No Color
+# Script Name: cPanel Mail Check
+# Author: Steven Fleming
+# Created: May 9thth 2024
+# Modified: May 9rd 2024
+# Version: 1.0
+
+# Define text colors
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+NC='\033[0m' # No Color
 
  # Check if sudo
 if [ "$EUID" -ne 0 ]; then
