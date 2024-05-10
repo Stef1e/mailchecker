ad59e9d13aceeb0a9259097e49aa91f593046b2e
Stef1e <154548870+Stef1e@users.noreply.github.com>
2024-05-10 12:59:43 -0400

    Update cP_MSP.sh
    added color codes and echo output of detected domains using whmapi1

--- cP_MSP.sh
+++ cP_MSP.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
 
-# Script Name: cPanel Mail Check
-# Author: Steven Fleming
-# Created: May 9thth 2024
-# Modified: May 9rd 2024
-# Version: 1.0
-
 # Define text colors
 RED='\033[0;31m'
 GREEN='\033[0;32m'
-YELLOW='\033[1;33m'
 NC='\033[0m' # No Color
 
- # Check if sudo
+# Check if sudo
 if [ "$EUID" -ne 0 ]; then
     echo -e "${RED}This script is required to be ran with sudo privileges!${NC}" 1>&2
     exit 1
@@ -22,11 +15,19 @@ echo "Running cPanel Mail Status Probe"
 
 sleep 1
 
-/usr/local/cpanel/3rdparty/bin/perl <(curl -s "https://raw.githubusercontent.com/CpanelInc/tech-SSE/master/msp.pl") --auth --conf --rbl --all --verbose --queue --maillog
+/usr/local/cpanel/3rdparty/bin/perl <(curl -s "https://raw.githubusercontent.com/CpanelInc/tech-SSE/master/msp.pl") --auth --conf --rbl --all --verbose --rude --queue --maillog
 
- read -p "Would you also like to check the domains? (y|n)" answer
+echo -e "${GREEN}Domains detected!${NC}"
 
- case $answer in
+sleep 0.5
+
+whmapi1 listaccts | grep domain | awk '{print $2}'
+
+sleep 0.5
+
+read -p "Would you like to check the domains? (y|n):" answer
+
+case $answer in
     yes|y)
         for accts in $(whmapi1 listaccts | grep domain | awk '{print $2}'); do
         echo "_______________________________________________________________________________________"
